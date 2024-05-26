bb7319c76a9e6638ffa6d66d9dad8a1f6da6cff6
hranswerguruji <hranswerguruji@gmail.com>
2024-05-26 09:48:44 +0530

    Change for 1. Setting title 2. Setting width and height

--- main.py
+++ main.py
@@ -2,4 +2,7 @@ import tkinter
 
 window = tkinter.Tk() # Initialization the tkinter
 
+window.title("Calculator") # Setting title
+window.minsize(width=500, height=300) # Setting width and height
+
 window.mainloop() # Display UI. This line will end of the program
