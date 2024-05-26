473005c273d1562f5c5e6f268770743f1f16b7d1
hranswerguruji <hranswerguruji@gmail.com>
2024-05-26 09:51:29 +0530

    Change for Property of label

--- main.py
+++ main.py
@@ -5,4 +5,10 @@ window = tkinter.Tk() # Initialization the tkinter
 window.title("Calculator") # Setting title
 window.minsize(width=500, height=300) # Setting width and height
 
+# Declare label
+lblHeading = tkinter.Label(text="Welcome to calculator program", font=("Arial",20, "bold"))
+lblHeading.pack()
+
+
+
 window.mainloop() # Display UI. This line will end of the program
