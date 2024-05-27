beb999c5f49ff9daf8815cfd0f858efcc25fc555
hranswerguruji <hranswerguruji@gmail.com>
2024-05-27 19:32:51 +0530

    Change for: Calculator program done

--- main.py
+++ main.py
@@ -1,18 +1,16 @@
-import tkinter
+from tkinter import *
 
-window = tkinter.Tk() # Initialization the tkinter
+window = Tk()  # Initialization the tkinter
 
-window.title("Calculator") # Setting title
-window.minsize(width=500, height=300) # Setting width and height
+window.title("Calculator")  # Setting title
+window.minsize(width=500, height=300)  # Setting width and height
 
 # Declare label
-lblHeading = tkinter.Label(text="Welcome to calculator program", font=("Arial",20, "bold"))
+lblHeading = Label(text="Welcome to calculator program", font=("Arial", 20, "bold"))
 lblHeading.pack(side="left")
 
-import turtle
-tim = turtle.Turtle()
-tim.write("Hell Santosh")
+# Declare button
+btnAdd=Button(text="Add")
+btnAdd.pack()
 
-
-
-window.mainloop() # Display UI. This line will end of the program
+window.mainloop()  # Display UI. This line will end of the program
