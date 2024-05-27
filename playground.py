7399ef62bf8ac1ba4a83447f15df7a19f250a23e
hranswerguruji <hranswerguruji@gmail.com>
2024-05-27 10:37:07 +0530

    Change for: **kwargs: Many keyword arguments

--- playground.py
+++ playground.py
@@ -1,7 +1,26 @@
 def add(*args):
-    sum=0
+    sum = 0
     for n in args:
-        sum+=n
+        sum += n
     return sum
 
-print(add(1,100))
+
+print(add(1, 100))
+
+
+def calculator(p, **kwargs):
+    p += kwargs["add"]
+    p -= kwargs["sub"]
+    p *= kwargs["mul"]
+    p /= kwargs["div"]
+
+    #for key, value in kwargs.items():
+    #print("Key: " + key + ", Value: " +str(value))
+    #print(kwargs["add"])
+    #p += kwargs["add"]
+    #result *= kwargs["mul"]
+    #result /= kwargs["div"]
+    return p
+
+
+print(calculator(4, add=10, sub=2, mul=5, div=2))
